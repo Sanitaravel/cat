8ba85abc005496023ef72f69a4c0ea8360f7af38
Alexbanan <SimCityrulit22@gmail.com>
2019-12-21 11:28:44 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Cats :fact = "factic"></Cats>
+  </div>
+</template>
+
+<script>
+import Cats from './components/Cats.vue'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      factic: ''
+    }
+  },
+  components: {
+    Cats
+  },
+  mounted() {
+    axios.get('http://188.225.47.187/api/cats/random-fact.php').then((response)=>{
+      this.factic = response.data;
+    },
+    setInterval(() => axios.get('http://188.225.47.187/api/cats/random-fact.php').then((response)=>{
+      this.factic = response.data;
+    }), 10000))
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Cats.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="factic">
+    <h2>Интересный факт</h2>
+    <p>
+      {{fact}}
+    </p>
+    <img src="img/kitty.png" alt="" />
+  </div>
+</template>
+
+<style>
+      body {
+        padding: 0;
+        margin: 0;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .factic {
+        width: 300px;
+        border-radius: 10px;
+        padding: 20px;
+        font-family: "Arial", sans-serif;
+        box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.5);
+        background-color: rgb(238, 248, 237);
+        text-align: center;
+        color: #3b4668;
+        cursor: pointer;
+        transition: 0.3s;
+      }
+
+      .factic:hover {
+        transform: scale(1.05);
+        box-shadow: 0px 0px 13px 0px rgba(50, 50, 50, 0.5);
+      }
+
+      .factic h2 {
+        font-size: 22px;
+        font-weight: 900;
+      }
+
+      .factic h2:after {
+        content: " ";
+        display: block;
+        width: 50px;
+        height: 3px;
+        background-color: #3b4668;
+        margin: 15px auto;
+      }
+
+      .factic p {
+        font-size: 16px;
+        color: #848aa0;
+      }
+</style>
+
+<script>
+  export default{  
+    name: 'FirstComponent',
+    props: ["fact"],
+    data(){
+      return {
+        }  
+      }
+    }
+</script>
